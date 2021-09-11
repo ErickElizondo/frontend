aa173f29b497d5e6b845e33681562b636cf9310d
ErickElizondo <erick.elizondo20@gmail.com>
2021-09-10 20:17:47 -0600

    c

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -20,6 +20,9 @@ const Login = ({ history }) => {
     const config = {
       header: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+        "Access-Control-Allow-Methods:":  "POST, GET, OPTIONS"
       },
     };
 
